7ec1fe96bc270904a9a39c42628140771b3fa6d3
mohamedmahmoud9 <mohamed0105050@gmail.com>
2024-10-17 16:38:29 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://4ac336f68642a08c0b099b1692278dab@o4508138134765568.ingest.de.sentry.io/4508138137387088';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      options.enableFramesTracking = true;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
+
+  // or define SENTRY_DSN via Dart environment variable (--dart-define)
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late String test;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Text(test),
+          Center(
+              child: ElevatedButton(
+            onPressed: () {
+              List.generate(100, (index) {
+                setState(() {
+                  print('hello $index');
+                });
+              });
+            },
+            child: const Text('Verify Sentry Setup'),
+          )),
+        ],
+      ),
+    );
+  }
+}
